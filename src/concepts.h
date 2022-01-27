a702d0759c9cb433438481e355da2ddbf4048ed8
aljo242 <alex.johnson@tendermint.com>
2022-01-27 10:28:18 -0500

    add concepts

---
+++ src/concepts.h
@@ -0,0 +1,16 @@
+#ifndef _CONCEPTS_H
+#define _CONCEPTS_H
+
+#include <numeric>
+#include <concepts>
+#include <vector>
+
+template <typename T>
+requires std::integral<T> || std::floating_point<T>
+constexpr double Average(std::vector<T> const &vec) {
+  // accumulate starting with init value of 0
+  const double sum = std::accumulate(vec.begin(), vec.end(), 0.0);
+  return sum / double(vec.size());
+}
+
+#endif // _CONCEPTS_H
